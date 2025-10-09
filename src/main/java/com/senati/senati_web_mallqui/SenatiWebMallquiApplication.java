9e9f23c1a4488e39e83c6c4ac5364b9c9286a985
Mayra Mallqui <158282403@senati.pe>
2025-10-09 09:55:14 -0500

    first commit

---
+++ src/main/java/com/senati/senati_web_mallqui/SenatiWebMallquiApplication.java
@@ -0,0 +1,13 @@
+package com.senati.senati_web_mallqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenatiWebMallquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenatiWebMallquiApplication.class, args);
+	}
+
+}
